7a733cca1801c90ad88cc8b40b927052d71dacd8
aliepba <aliefbagas04@gmail.com>
2024-11-04 11:34:02 +0700

    delete unused views

--- resources/views/views/includes/user/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="footer text-muted">
-    &copy; 2021. <a href="#">Lembaga Pengembangan Jasa Konstruksi</a>
-</div>
-
--- resources/views/views/includes/v2/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12 footer-copyright text-center">
-          <p class="mb-0">Lembaga Pengembangan Jasa Konstruksi 2023</p>
-        </div>
-      </div>
-    </div>
-</footer>
